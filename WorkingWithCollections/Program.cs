3071137c0865c6a7f0673afa3556a2bd04f978f9
manel talbi <talbimanel20@gmail.com>
2022-01-12 10:58:11 +0100

    ArrayList

---
+++ WorkingWithCollections/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+namespace WorkingWithCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "oldmobile";
+            car1.Model = "cutlas supreme";
+
+            Car car2 = new Car();
+            car2.Make = "Geo";
+            car2.Model = "Prism";
+
+            Book b1 = new Book();
+            b1.Author = "Robert Tabor";
+            b1.Title = "Microsoft .!net XML Web Services";
+            b1.ISBN = "0-000-00000-0";
+
+            // ArrayLists are dynamically sized,
+            // cool features sorting,remove items
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add(car1);
+            myArrayList.Add(car2);
+            myArrayList.Add(b1);
+            myArrayList.Remove(b1);
+            foreach (Car car in myArrayList)
+            {
+                Console.WriteLine(car.Make);
+            }
+            Console.ReadLine();
+
+        }
+    }
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+    }
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
